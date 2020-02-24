cafa8b9ebb3e113d805a543ae18f21403d63a306
Walter <w.e.p.l.91@gmail.com>
2020-02-24 15:19:21 -0300

    Add and remove file in case we want to upload more than onefile

--- src/components/Upload.jsx
+++ src/components/Upload.jsx
@@ -1,8 +1,9 @@
 import './styles/upload.css';
 import React, { Component } from 'react';
 
-import { Row, Form } from "react-bootstrap";
 import Button from '@material-ui/core/Button';
+import DeleteIcon from '@material-ui/icons/Delete';
+import IconButton from '@material-ui/core/IconButton';
 import ReactFileReader from 'react-file-reader';
 
 import CareerSelect from '../components/CareerSelect';
@@ -14,46 +15,70 @@ class Upload extends Component {
     super(props);
 
     this.state = {
-      fileSelected: null,
+      filesSelected: [],
       career: null,
     }
 
   }
-    /**
-     * Methods
-    */
-    upLoad = () => {
-      
-    }
 
-    loadFile = () => {
-      const reader = new FileReader();
-
-      reader.onload(() => {})
-    }
-
-    render() {
-      const { career } = this.state;
+  renderLabel() {
+    const { filesSelected } = this.state;
+    if (!filesSelected || !filesSelected.length) {
       return(
-        <div className="upload-form-container">
-          <div className="form-selects-row">
-            <CareerSelect 
-              onChange={(event, newCareer) => this.setState({ career: newCareer, isLoading: true })} />
-            <AssignatureSelect
-              careerId={career ? career.Id : null}
-              onChange={(event, newAssignature) => this.setState({ assignature: newAssignature })} />
-          </div>
-          <div className="from-action-container">
-            <label htmlFor="real-apunte-btn">Seleccione archivo</label><br/>
-            <ReactFileReader handleFiles={(files) => this.loadFile(files)}>
-              <Button type="file" variant="contained" color="primary">Buscar..</Button>
-            </ReactFileReader>
-            <br/>
-            <div className="float-right">
-              <Button variant="contained" color="secondary" onClick={() => this.upLoad()}>Subir</Button>
-            </div>      
-          </div>
-        </div>);
+        <React.Fragment>
+          <label htmlFor="real-apunte-btn">Seleccione archivo</label>
+          <br/>
+        </React.Fragment>)
+    }
+    return (
+      <React.Fragment>
+        {filesSelected.map(file =>
+          <React.Fragment>
+            <label>{file.name}</label>
+            <IconButton 
+              color="primary"  
+              component="span"
+              onClick={() => this.deleteFile(file)}>
+              <DeleteIcon />
+            </IconButton>
+            <br />
+          </React.Fragment> )}
+        <label>Selecciona otro archivo</label>
+      </React.Fragment>)
+  }
+
+  deleteFile(file) {
+    const { filesSelected } = this.state;
+    const copiedFiles = Array.from(filesSelected);
+    const filteredFiles = copiedFiles.filter(element => element.name != file.name);
+    
+    this.setState({
+      filesSelected: filteredFiles,
+    });
+  }
+
+  render() {
+    const { career } = this.state;
+    return(
+      <div className="upload-form-container">
+        <div className="form-selects-row">
+          <CareerSelect 
+            onChange={(event, newCareer) => this.setState({ career: newCareer, isLoading: true })} />
+          <AssignatureSelect
+            careerId={career ? career.Id : null}
+            onChange={(event, newAssignature) => this.setState({ assignature: newAssignature })} />
+        </div>
+        <div className="from-action-container">
+          { this.renderLabel() }
+          <ReactFileReader handleFiles={(files) => this.setState(prevState => ({ filesSelected: [...files, ...prevState.filesSelected] }))}>
+            <Button type="file" variant="contained" color="primary">Buscar..</Button>
+          </ReactFileReader>
+          <br/>
+          <div className="float-right">
+            <Button variant="contained" color="secondary" onClick={() => this.upLoad()}>Subir</Button>
+          </div>      
+        </div>
+      </div>);
     }
 }
 
