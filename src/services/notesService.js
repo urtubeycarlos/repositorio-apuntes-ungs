95483eed37c272a26611d0920f4a8704fac5e208
Walter <w.e.p.l.91@gmail.com>
2020-02-24 15:51:38 -0300

    Add note service to results view, missing API method?

--- src/services/notesService.js
+++ src/services/notesService.js
@@ -2,4 +2,4 @@ import RESTClient from './RESTClient';
 const restclient = new RESTClient();
 
 export const getAllNotes = () => restclient.get('note');
-export const getNoteByAssignaure = (assignatureId) => restclient.get('assignature', null, { assignatureid: assignatureId });
+export const getNoteByAssignaure = (assignatureId) => restclient.get('note', null, { assignatureid: assignatureId });
--- src/views/Results.jsx
+++ src/views/Results.jsx
@@ -4,42 +4,74 @@ import Loading from "../components/Loading.jsx";
 import Logo from '../components/Logo';
 import Note from '../components/Note';
 
-export default class Results extends Component {
-    
-    constructor(props){
-        super(props)
-        this.state = {
-            results: null
-        }
+const { getNoteByAssignaure } = require('../services/notesService');
+
+class Results extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        results: null
+      }
+  }
+
+  componentDidMount() {
+    const searchParams = this.getParams();
+    debugger
+    if (searchParams.assignature) {
+      getNoteByAssignaure(searchParams.assignature)
+        .then(response => {
+          debugger
+          this.setState({
+            results: response.data.Notes,
+          });
+        })
+        .catch((err) => {
+          debugger
+          //handle somehow this error
+        });
+    }
+  }
+
+  getParams() {
+    const params = {};
+    const searchParams = new URLSearchParams(window.location.search)
+
+    for (let param of searchParams) {
+      params[param[0]] = param[1];
     }
 
-    render() {
-        const { results } = this.state;
-        return !results ?
-            <Container>
-                <Logo />
-                <Jumbotron>
-                    <Row className="justify-content-center">
-                        <h5>Buscando apuntes</h5>
-                    </Row>
-                    <Loading />
-                    <Note 
-                        filename={'complejidad_computacional'} 
-                        extension={'.java'} 
-                        url={"https://www.youtube.com/"} 
-                        description={"Soy una describicion."}/>   
-                </Jumbotron>
-            </Container> :
-            <Container>
-                <Logo />
-                <Jumbotron>
-                    <Row className="justify-content-center">
-                        <h5>Apuntes encontrados</h5>
-                    </Row>
-                </Jumbotron>
-            </Container>
-        }
-    }
+    return params
+  }
+
+  render() {
+    const { results } = this.state;
+    return !results ?
+      <Container>
+        <Logo />
+        <Jumbotron>
+          <Row className="justify-content-center">
+            <h5>Buscando apuntes</h5>
+          </Row>
+          <Loading />
+          <Note 
+            filename={'complejidad_computacional'} 
+            extension={'.java'} 
+            url={"https://www.youtube.com/"} 
+            description={"Soy una describicion."}/>   
+        </Jumbotron>
+      </Container> :
+      <Container>
+        <Logo />
+        <Jumbotron>
+          <Row className="justify-content-center">
+            <h5>Apuntes encontrados</h5>
+          </Row>
+        </Jumbotron>
+      </Container>
+  }
+}
+
+export default Results;
 
     // componentWillMount(){
     //     var self = this;
