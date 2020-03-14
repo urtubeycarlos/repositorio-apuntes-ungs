c1ee5e14ecb632024cf1ac8cf92fc0453c83ff9c
Carlos Urtubey <urtubeycarlos.0510@outlook.com>
2020-03-14 11:53:48 -0300

    Fixed get assignatures

--- src/services/assignaturesService.js
+++ src/services/assignaturesService.js
@@ -3,4 +3,4 @@ const restclient = new RESTClient();
 
 export const getAllAssignatures = () => restclient.get('assignature');
 export const getAssignature = (assignatureId) => restclient.get('assignature', assignatureId);
-export const getAssignatureByCareer = (careerId) => restclient.get('assignature', null, { careerid: careerId });
+export const getAssignatureByCareer = (careerId) => restclient.get('assignature', careerId);
